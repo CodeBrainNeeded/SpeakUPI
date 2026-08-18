e22b54fc82bd57099b223cb1c3146449e80a0949
CodeBrainNeeded <95329066+CodeBrainNeeded@users.noreply.github.com>
2026-08-18 00:44:13 -0700

    fixed the release file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.speakupi"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "1.0"
 
         ndk {
             abiFilters += listOf("armeabi-v7a", "arm64-v8a")
